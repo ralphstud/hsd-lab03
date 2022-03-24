944d7f25467232ffb1356e8da09df9ccb957d8ad
ralphnation <ofbyforralph@gmail.com>
2022-03-25 07:12:19 +0900

    add skeleton

---
+++ my_fusedmult.v
@@ -0,0 +1,12 @@
+module my_fusedmult #(
+    parameter BITWIDTH = 32
+)
+(
+    input [BITWIDTH-1:0] ain,
+    input [BITWIDTH-1:0] bin,
+    input en,
+    input clk,
+    output [2*BITWIDTH-1:0] dout
+);
+/* IMPLEMENT HERE! */
+endmodule
