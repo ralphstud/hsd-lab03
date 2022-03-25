4e9239ade0de60af0356458789aa8e0b08ab429c
thdduddn96 <thdduddn96@snu.ac.kr>
2022-03-25 09:56:22 +0900

    Update my_fusedmult.v

--- hsd-lab03.srcs/sources_1/new/my_fusedmult.v
+++ hsd-lab03.srcs/sources_1/new/my_fusedmult.v
@@ -6,7 +6,16 @@ module my_fusedmult #(
     input [BITWIDTH-1:0] bin,
     input en,
     input clk,
-    output [2*BITWIDTH-1:0] dout
-);
+    output [2*BITWIDTH-1:0] dout,
+    
+    reg [2*BITWIDTH-1:0] out
+    );
 /* IMPLEMENT HERE! */
+
+always @(posedge clk) begin
+    out = 0;
+    if (en) out = ain * bin;    
+end
+
+assign dout = out;
 endmodule
